801e98597ef9e4087ab5797a5758a0e6974fcc6d
Samuel Toshikazu Oizume <samuel.oizume@gmail.com>
2020-02-08 03:11:40 -0300

    segundo
    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,20 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  allCharacteres: Observable<any>;
-  allSeries: Observable<any>; 
-  allComics: Observable<any>;
   
   PRIVATE_KEY : string;
   HASH : string;
+
+  page: number = 1;
+  offset: number;
+  limit: number = 20; 
+  total: number; 
+  total_pages: number;
+
+  comics: [];
+
+  private loading_series: boolean = false;
+  private loading_comics: boolean = false;
 
   constructor(private api: MarvelApiService) { }
 
@@ -27,19 +34,74 @@ export class HomeComponent implements OnInit {
     this.HASH = this.api.HASH;
   }
 
-  getAllCharacteres()
-  {
-    this.allCharacteres = this.api.getAllCharacteres();
-  }
-
   getAllSeries()
   {
-    this.allSeries = this.api.getAllSeries();
+    this.loading_series = true;
+    this.api.getAllSeries().then(r => {
+      this.loading_series = false;
+    });
   }
 
   getAllComics()
   {
-    this.allComics = this.api.getAllComics();
+    this.loading_comics = true;
+    this.api.getAllComics().then(r => {
+      this.loading_comics = false;
+
+      this.limit = this.api.limit; 
+      this.total = this.api.total;
+      this.total_pages = Math.round(this.total / this.limit);
+    });
+  }
+
+  onChange()
+  {
+    this.loading_comics = true;
+    var offset = (this.page - 1) * this.limit;
+    this.api.getComics(this.limit,offset).then(r => {
+      this.loading_comics = false;
+      this.limit = this.api.limit; 
+      this.total = this.api.total;
+      this.total_pages = Math.round(this.total / this.limit);
+    });
+  }
+  
+  onSelectLimite(valor)
+  {
+    this.loading_comics = true;
+    var v = parseInt(valor);
+    this.limit = v;
+    var offset = (this.page - 1) * v;
+    this.api.getComics(v,offset).then(r => {
+      this.loading_comics = false;
+      this.limit = this.api.limit; 
+      this.total = this.api.total;
+      this.total_pages = Math.round(this.total / this.limit);
+    });
+  }
+
+  onClickPrevious()
+  {
+    if(this.page > 0)
+    { 
+      this.page--;
+    } 
+    this.onChange();
+  }
+
+  onClickNext()
+  {
+    if(this.page < this.total_pages)
+    {
+      this.page++;
+    }
+    this.onChange();
+  }
+
+  info(c)
+  {
+    console.log(c);
+    this.comics = c.comics.items;
   }
 
 }
--- src/app/service/marvel-api.service.ts
+++ src/app/service/marvel-api.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 import { HttpClient} from '@angular/common/http';
-import {Md5} from 'ts-md5/dist/md5';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,32 +10,98 @@ export class MarvelApiService {
   PRIVATE_KEY = '55bb95924325d4c8f980f3465a7cecc7912e3b4b';
   PUBLIC_KEY  = 'd9e986bc36dff925ac8af63f62037bcd';
   HASH = '6f27cda563f66088a8b6da9e63b9e2bb'; 
-
   URL_API = `https://gateway.marvel.com/v1/public/{method}?ts=2&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}`;
+
+
+  series_results:  Observable<any>;
+  comics_results: Observable<any>;
+
+  offset: number;
+  limit: number; 
+  total: number; 
 
   constructor(private http:HttpClient) {}
 
-  getAllCharacteres():  Observable<any>
-  {
-      this.URL_API = this.URL_API.replace("{method}","characters");
+  getAllSeries(){
+
+    let promise = new Promise((resolve, reject) => {
+      this.URL_API = this.URL_API.replace("{method}", "series"); 
       console.log(this.URL_API);
-      return this.http.get<any>(this.URL_API).pipe(map((data: any) => data.data.results));
+      this.http.get<any>(this.URL_API)
+      .toPromise()
+      .then(
+        res => {
+          this.series_results = res.data.results;
+          resolve();
+        },
+        msg => {
+          reject(msg);
+        }
+      );
+    });
+    return promise;
   }
 
-
-  getAllSeries(): Observable<any>
+  getComics(limit, offset)
   {
-    this.URL_API = this.URL_API.replace("{method}", "series"); 
-    console.log(this.URL_API);
-    return this.http.get<any>(this.URL_API).pipe(map((data: any) => data.data.results));
+    let  promise = new Promise((resolve, reject) => {
+        this.URL_API = this.URL_API.replace("{method}", "comics");
+        var URL_WITH_FILTER = this.URL_API;
+
+        if(limit > 0)
+        {
+          URL_WITH_FILTER =  URL_WITH_FILTER + "&limit=" + limit;
+        }
+
+        if(offset > 0)
+        {
+          URL_WITH_FILTER = URL_WITH_FILTER + "&offset=" + offset;
+        }
+        console.log(URL_WITH_FILTER);
+        this.http.get<any>(URL_WITH_FILTER)
+        .toPromise()
+        .then(
+          res => {
+            this.limit = res.data.limit;
+            this.offset = res.data.offset;
+            this.total = res.data.total;
+          
+            console.log(res.data.results.length);
+            this.comics_results = res.data.results;
+            resolve();
+          },
+          msg => {
+            reject(msg);
+          }
+
+        )
+    });
+    return promise;
   }
 
-  getAllComics(): Observable<any>{
+  getAllComics(){
+    let promise = new Promise((resolve, reject) => {
+      this.URL_API = this.URL_API.replace("{method}", "comics");
+      console.log(this.URL_API);
+      this.http.get<any>(this.URL_API)
+    .toPromise()
+    .then( 
+        res => {
+          this.limit = res.data.limit;
+          this.offset = res.data.offset;
+          this.total = res.data.total;
 
-    this.URL_API = this.URL_API.replace("{method}", "comics");
-    console.log(this.URL_API);
-    return this.http.get<any>(this.URL_API).pipe(map((data: any) => data.data.results));
+          this.comics_results = res.data.results;
+          resolve();
+        },
+        msg => {
+          reject(msg);
+        }
+      )
+    });
+    return promise;
   }
+
   
-
+  
 }
